1fee0799867693ac2f0eca0c0608f4247dcfd614
A7MED Raid <49642513+raidkarki@users.noreply.github.com>
2024-05-16 10:04:37 +0100

    Update config.sh
    changing in :sudo apt-get install -y --allow-change-held-packages kubelet kubeadm

--- scripts/config.sh
+++ scripts/config.sh
@@ -85,7 +85,7 @@ curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
-sudo apt-get install -y kubelet kubeadm
-sudo apt-mark hold kubelet kubeadm 
+sudo apt-get install -y --allow-change-held-packages kubelet kubeadm
+sudo apt-mark hold kubelet kubeadm kubectl
 
 sudo systemctl enable --now kubelet
